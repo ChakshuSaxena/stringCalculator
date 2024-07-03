423994c71349e43eb04d72877c0c31616c0d33af
chakshu saxena <chakshusaxena.cse@gmail.com>
2024-07-03 17:46:53 +0530

    add testcases

--- stringCalculator.rb
+++ stringCalculator.rb
@@ -19,34 +19,3 @@ def addString(numbers)
   sum += negative ? -current_no.to_i : current_no.to_i
   sum
 end
-
-# ------************* TestCases ***********---------------------
-
-# Rspec.describe '#addString' do
-#   it 'handle condition if number string is empty' do
-#     expect(addString("")).to eq(0)
-#   end
-
-#   it 'handle condition if number string is having positive no.' do
-#     expect(addString("1")).to eq(1)
-#   end
-
-#   it 'handle condition if number string is having positive number with comma(,)' do
-#     expect(addString("1,2")).to eq(3)
-#   end
-
-#   it 'handle condition if number string is having positive number with newline(\n)' do
-#     expect(addString("1,\n2")).to eq(3)
-#   end
-
-#   it 'handle condition if number string is having negative number' do
-#     expect(addString("-1")).to eq(-1)
-#   end
-#     it 'handle condition if number string is having negative number with comma(,)' do
-#     expect(addString("-1,-2")).to eq(-3)
-#   end
-
-#   it 'handle condition if number string is having negative number with newline(\n)' do
-#     expect(addString("-1,\n-2")).to eq(-3)
-#   end
-# end
